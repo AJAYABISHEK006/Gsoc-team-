839eb60427ec03b7baffab7013f6f5e3b85f92e3
Dharshni <dharshni0806@gmail.com>
2026-08-14 13:51:48 +0530

    Intro-OOPS

---
+++ OOPs/OOPS-1/pandas_math.py
@@ -0,0 +1,115 @@
+def addition(a,b):
+    return a+b
+
+def multiply(c,d):
+    return c*d
+
+def subtract(e,f):
+    return e-f
+
+def divide(x,y):
+    return x/y
+
+def floordiv(x,y):
+    return x//y
+def addition(a,b):
+    return a+b
+
+def multiply(c,d):
+    return c*d
+
+def subtract(e,f):
+    return e-f
+
+def divide(x,y):
+    return x/y
+
+def floordiv(x,y):
+    return x//y
+
+def addition(a,b):
+    return a+b
+
+def multiply(c,d):
+    return c*d
+
+def subtract(e,f):
+    return e-f
+
+def divide(x,y):
+    return x/y
+
+def floordiv(x,y):
+    return x//y
+def addition(a,b):
+    return a+b
+
+def multiply(c,d):
+    return c*d
+
+def subtract(e,f):
+    return e-f
+
+def divide(x,y):
+    return x/y
+
+def floordiv(x,y):
+    return x//y
+
+def addition(a,b):
+    return a+b
+
+def multiply(c,d):
+    return c*d
+
+def subtract(e,f):
+    return e-f
+
+def divide(x,y):
+    return x/y
+
+def floordiv(x,y):
+    return x//y
+def addition(a,b):
+    return a+b
+
+def multiply(c,d):
+    return c*d
+
+def subtract(e,f):
+    return e-f
+
+def divide(x,y):
+    return x/y
+
+def floordiv(x,y):
+    return x//y
+
+def addition(a,b):
+    return a+b
+
+def multiply(c,d):
+    return c*d
+
+def subtract(e,f):
+    return e-f
+
+def divide(x,y):
+    return x/y
+
+def floordiv(x,y):
+    return x//y
+def addition(a,b):
+    return a+b
+
+def multiply(c,d):
+    return c*d
+
+def subtract(e,f):
+    return e-f
+
+def divide(x,y):
+    return x/y
+
+def floordiv(x,y):
+    return x//y
---
+++ OOPs/OOPS-1/test.py
@@ -0,0 +1,5 @@
+import pandas_math as pdm
+
+print(pdm.multiply(8,9))
+
+print(pdm.divide(2384054,890342))
