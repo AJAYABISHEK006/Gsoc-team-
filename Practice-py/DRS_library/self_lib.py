f2517e96d5836fa908bda33f1856babf8e9cb5a3
dharsh03rs-cpu <dharsh03rs@gmail.com>
2026-08-19 11:36:59 +0530

    Creating own module

---
+++ Practice-py/DRS_library/self_lib.py
@@ -0,0 +1,8 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+# create as many own functions as possible
+#Can use this by importing file name
---
+++ Practice-py/DRS_library/text.py
@@ -0,0 +1,4 @@
+import self_lib as slb
+print(slb.add(30,20))
+print(slb.mul(12,2))
+print(slb.sub(100,50))
