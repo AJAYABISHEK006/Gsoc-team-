c95804a7789e4b82fa340dead31f89d1d9df72cd
SARATHI-78 <sundarasarathi78@gmail.com>
2026-09-08 15:11:37 +0530

    Pushed RecursionSumDigits.java file

---
+++ DSA/Recursion/RecursionSumDigits.java
@@ -0,0 +1,17 @@
+class RecursionSumDigits
+{
+    static int sumDigits(int n)
+    {
+        if(n == 0)
+        {
+            return 0;
+        }
+        return (n%10) + sumDigits(n/10);
+    }
+
+    public static void main(String[] args)
+    {
+        int n = 12345;
+        System.out.print(sumDigits(n));
+    }
+}
