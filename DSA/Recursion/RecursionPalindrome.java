a6bdbf456982873e9d34dba8e0f9a3c102e9c73e
SARATHI-78 <sundarasarathi78@gmail.com>
2026-09-09 15:21:10 +0530

    RecursionPalindrome.java

--- DSA/Recursion/RecursionPalindrome.java
+++ DSA/Recursion/RecursionPalindrome.java
@@ -1,13 +1,29 @@
 class RecursionPalindrome
 {
-    staic String palindrome
+    static String palindrome(int n)
     {
-        if()
+
+        if(reverse(n,0) == n)
+        {
+            return "Paindrome";
+        }   
+        else{
+            return "Not A Palindrome";
+        }
+    }
+
+    static int reverse(int n, int rev)
+    {
+        if(n == 0){
+            return rev;
+        }
+        rev = rev*10 + n%10;
+        return reverse(n/10,rev); 
     }
 
     public static void main(String[] args)
     {
         int n = 121;
-        palindrome(n);
+        System.out.print(palindrome(n));
     }
 }
